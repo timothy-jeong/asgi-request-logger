22b495f3955a0ec84706f8f835164da8c6d09622
timothy <k.jts8257@gmail.com>
2025-04-02 21:52:32 +0900

    fix: module import error

--- asgi_request_logger/__init__.py
+++ asgi_request_logger/__init__.py
@@ -1,4 +1,4 @@
 from .json_request_logger_middleware import JsonRequestLoggerMiddleware
-from logger import get_logger, get_queue_logger
+from .logger import get_logger, get_queue_logger
 
 __all__ = [JsonRequestLoggerMiddleware, get_logger, get_queue_logger]
